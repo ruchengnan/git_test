81e9db5ef2c9c992ef128fad2afb82c7eeeb2cf8
ruchengnan <843502824@qq.com>
2019-09-22 16:40:27 +0800

    增加修改2次

--- idea_git/src/main/java/GitPush.java
+++ idea_git/src/main/java/GitPush.java
@@ -2,5 +2,6 @@ public class GitPush {
     public static void main(String[] args) {
         System.out.println("推送测试");
         System.out.println("修改1");
+        System.out.println("修改2次");
     }
 }
